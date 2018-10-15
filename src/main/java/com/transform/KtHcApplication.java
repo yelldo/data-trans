e34183fd6b32abe55841626c4aa8c9e9ebc99e72
yelldo <799767342@qq.com>
2018-10-15 21:00:39 +0800

    init

---
+++ src/main/java/com/transform/KtHcApplication.java
@@ -0,0 +1,12 @@
+package com.transform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtHcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtHcApplication.class, args);
+	}
+}
