9425cd125e8296ed1413b2942246a808b60fb997
yelldo <799767342@qq.com>
2018-10-16 18:08:15 +0800

    添加工具类：spring容器中beans.name的查看

---
+++ src/main/java/com/transform/utils/LookUpSpringBean.java
@@ -0,0 +1,31 @@
+package com.transform.utils;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by tianhc on 2018/10/16.
+ */
+@RestController
+@RequestMapping("utils/springBean")
+public class LookUpSpringBean implements ApplicationContextAware{
+
+    private ApplicationContext applicationContext;
+
+    @RequestMapping("list")
+    public List springBeanList() {
+        String[] beans = applicationContext.getBeanDefinitionNames();
+        return Arrays.asList(beans);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+}
